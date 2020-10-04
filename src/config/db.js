c27e3d22a1b363e84c94fdd95b9bb2a0bbc2db26
unknown <idanhonda125@gmail.com>
2020-10-04 08:30:47 +0300

    Initialize project using Create React App

---
+++ src/config/db.js
@@ -0,0 +1,6 @@
+export var firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASRURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+  };
